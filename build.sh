2d35eb3fa1b539d6ff6f0b629ca5da94dea149d9
Distortions81 <carlotto81@gmail.com>
2023-10-02 16:28:16 -0600

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o CLImgExport-linux-amd64
-GOOS=windows GOARCH=amd64 go build -o CLImgExport-windows-amd64.exe
-GOOS=darwin GOARCH=amd64 go build -o CLImgExport-osx-amd64
-GOOS=darwin GOARCH=arm64  go build -o CLImgExport-osx-arm64
+GOOS=linux GOARCH=amd64 go build -o CLImgExport
+zip CLImgExport-linux-amd64.zip CLImgExport 
+GOOS=windows GOARCH=amd64 go build -o CLImgExport.exe
+zip CLImgExport-windows-amd64.zip CLImgExport.exe
+GOOS=darwin GOARCH=amd64 go build -o CLImgExport
+zip CLImgExport-mac-amd64.zip CLImgExport 
+GOOS=darwin GOARCH=arm64  go build -o CLImgExport
+zip CLImgExport-mac-m1.zip CLImgExport
+rm CLImgExport
+rm CLImgExport.exe
